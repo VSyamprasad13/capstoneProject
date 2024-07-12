dd4a041fd6487a3b4e2b9bf58016db780a3637cd
VSyamprasad13 <166799132+VSyamprasad13@users.noreply.github.com>
2024-07-12 14:38:02 +0530

    Update invemtory.service.ts

--- invemtory.service.ts
+++ invemtory.service.ts
@@ -1,1 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { InventoryItem } from '../models/inventory-item.model';
 
+@Injectable({
+  providedIn: 'root'
+})
+export class InventoryService {
+  private apiUrl = 'http://localhost:8080/api/inventory'; // Adjust this URL to your backend
+
+  constructor(private http: HttpClient) {}
+
+  getAllItems(): Observable<InventoryItem[]> {
+    return this.http.get<InventoryItem[]>(this.apiUrl);
+  }
+
+  addItem(item: InventoryItem): Observable<InventoryItem> {
+    return this.http.post<InventoryItem>(this.apiUrl, item);
+  }
+
+  updateItem(item: InventoryItem): Observable<InventoryItem> {
+    return this.http.put<InventoryItem>(`${this.apiUrl}/${item.id}`, item);
+  }
+
+  deleteItem(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
